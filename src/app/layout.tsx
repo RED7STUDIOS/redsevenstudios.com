d2e452d079f178823762631ac1c5b56c4448ab5c
Mitchell <68230846+Creaous@users.noreply.github.com>
2026-03-02 21:14:49 +1030

    Refactor RootLayout by removing unused components
    Removed unused imports and metadata, simplified layout structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,3 @@
-import { Navbar } from '@/components/navbar';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import { cn } from '@/lib/utils';
-import type { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
-import './globals.css';
-
-const fontSans = FontSans({
-	subsets: ['latin'],
-	variable: '--font-sans'
-});
-
-export const metadata: Metadata = {
-	title: 'RED7 STUDIOS',
-	description: 'RED7 STUDIOS is an Australian technology company.'
-};
-
 export default function RootLayout({
 	children
 }: Readonly<{
@@ -22,25 +5,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<head>
-				<link rel="icon" href="/favicon.ico" sizes="any" />
-				<link
-					rel="icon"
-					href="/favicon.svg"
-					type="image/svg"
-					sizes="any"
-				/>
-			</head>
-			<body
-				className={cn(
-					'min-h-screen h-screen bg-background font-sans antialiased dark',
-					fontSans.variable
-				)}
-			>
-				<TooltipProvider>
-					<Navbar />
-					{children}
-				</TooltipProvider>
+			<body>
+				<p>Nothing to see here...</p>
 			</body>
 		</html>
 	);
